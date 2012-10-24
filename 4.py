d88dea8cc55987314c58f717b5fb9dbb9ca3c9ca
Zach Dexter <zach@databracket.com>
2012-10-24 17:36:05 -0400

    Refactored #4.

--- 4.py
+++ 4.py
@@ -1,47 +1,69 @@
 #!/usr/bin/env python
 
-def find_largest_palindrome_product(n):
+import doctest
+import math
+
+def find_largest_palindrome_product(n,verbose=False):
     """Returns largest palindrome made from product of
         two n-digit numbers
+    >>> find_largest_palindrome_product(2)
+    9009
     """
-    palindromes = find_all_palindromes(n*2)
-    print palindromes
+    palindromes = find_all_palindromes(n,verbose)
+    if verbose: print palindromes
     for p in reversed(palindromes):
         # Return the first p for which there are
         #   two n-digit factors
         factor1 = 10**n - 1
         for i in range(factor1, 10**(n-2), -1):
             if p % i == 0 and p / i <= factor1:
-                print '{} divides {} {} times.'.format(
+                if verbose: print '{} divides {} {} times.'.format(
                     i, p, p/i
                 )
                 return p
-    
-def find_all_palindromes(n_original):
-    """Returns all n-digit numerical palindromes.
+
+def try_another_digit(myList):
+    """Return all possible numbers you can get by
+        adding 1 digit to every number in myList
+    >>> try_another_digit(["1", "2"])
+    ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29']
     """
-    assert n_original > 1
-    import math
-    n = int(math.floor(n_original / 2))
-    end_digit_choices = range(1, 10)
-    num_middle_digits = n - 1
-    middle_digit_choices = range(10)
-    palindromes = []
-    
-    for c in end_digit_choices:
-        palindrome = c * 10**(n-1)
-        for x in range(num_middle_digits):
-            for d in middle_digit_choices:
-                palindromes.append(
-                    palindrome + d * 10**(x)
-                )
-    palindromes = [int(str(x) + str(x)[::-1]) for x in palindromes]
-    print 'Found {} {}-digit palindromes.'.format(
+    numbers = "0123456789"
+    new_numbers = []
+    for entry in myList:
+        for num in numbers:
+            new_numbers.append(
+                entry + num
+            )
+    return new_numbers
+
+def get_half_palindromes(half_palindrome_length):
+    """
+    >>> len(get_half_palindromes(3))
+    900
+    """
+    if half_palindrome_length == 1:
+        return list("123456789")
+    return try_another_digit(get_half_palindromes(half_palindrome_length-1))  
+ 
+def find_all_palindromes(num_factor_digits,verbose=False):
+    """Returns all numerical palindromes of length num_factor_digits * 2.
+    >>> len(find_all_palindromes(2))
+    90
+    >>> len(find_all_palindromes(3))
+    900
+    """
+    assert num_factor_digits > 1
+    end_digit_choices = "123456789"
+     
+    palindromes = get_half_palindromes(num_factor_digits)
+    palindromes = [int(x + x[::-1]) for x in palindromes]
+    if verbose: print 'Found {} {}-digit palindromes.'.format(
         len(palindromes),
         n_original
         )
     return palindromes
 
 if __name__ == '__main__':
-    assert find_largest_palindrome_product(2) == 9009
-    print find_largest_palindrome_product(3)
+    doctest.testmod() 
+    print find_largest_palindrome_product(4)
